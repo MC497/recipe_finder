6de3668d419fb709366c633ec1dc96ec049c17b7
Madison Chan <madisonchan17@gmail.com>
2024-04-22 18:12:52 -0400

    Recipe details page

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -41,7 +41,6 @@ const Home = () => {
                     }
                 </tbody>
             </table>
-            <button className="btn btn-primary">Hello</button>
         </div>
         </>
     )
--- client/src/views/RecipeDetails.jsx
+++ client/src/views/RecipeDetails.jsx
@@ -1,7 +1,61 @@
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {Link} from "react-router-dom";
+
 const RecipeDetails = () => {
+
+    const [recipe,setRecipe] = useState({});
+
+    const {id} = useParams();
+
+    useEffect(() => {
+        axios.get(`http://localhost:9999/api/recipes/${id}`)
+            .then((res) => {
+                console.log(res.data);
+                setRecipe(res.data)
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    },[id])
+
+    const makeIngredientList = (importIngrList) => {
+        // console.log(importIngrList);
+        const ingrList_fin=[];
+        for (let i=0; i<importIngrList.length; i++) {
+            ingrList_fin.push(<li key={i}>{importIngrList[i]}</li>);
+        }
+        return ingrList_fin;
+        }
+
+        const makeStepList = (importstepList) => {
+            const stepList_fin=[];
+            for (let i=0; i<importstepList.length; i++) {
+                stepList_fin.push(<li key={i}>{importstepList[i]}</li>);
+            }
+            return stepList_fin;
+            }
 
     return (
         <>
+        <div className="detailsContainer">
+            <h1>{recipe.title}</h1>
+            <span><h6>Time: </h6> <p>{recipe.time}</p></span>
+            <h6>Cuisine: </h6><p>{recipe.cuisine}</p>
+
+            <div className="card">
+            <h3>Ingredients</h3>
+            <ul>{recipe.ingredients && makeIngredientList(recipe.ingredients)}</ul>
+            </div>
+            
+            <div className="card">
+                <h3>Steps</h3>
+                <ol>{recipe.steps && makeStepList(recipe.steps)}</ol>
+            </div>
+            
+            <Link to="/" className="btn btn-primary">Back to Main Page</Link>
+        </div>
         </>
     )
 }
